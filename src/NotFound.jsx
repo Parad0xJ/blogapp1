a2849a238926b81038625216d03d6d8ce52111b8
Parad0xJ <laurent-lyon@live.fr>
2025-01-24 20:24:46 +0100

    modif NotFound page

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -6,8 +6,10 @@ export default function NotFound() {
       <h3 className="text-bg-warning text-center pt-10">
         Cette page n'existe pas ! 4️⃣0️⃣4️⃣{" "}
       </h3>
-      <div className="text-center">
-        <Link to="/">Retournez sur la HOME - PAGE</Link>
+      <div className="text-center mt-5">
+        <Link to="/" style={{ color: "white" }}>
+          Retournez sur la HOME - PAGE
+        </Link>
       </div>
     </div>
   );
